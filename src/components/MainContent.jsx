61aad79e0df4c7a6209c03f0f969e269779531c3
Gustavo Carvalho <gustavohs.carvalho280590@gmail.com>
2024-06-18 20:38:29 -0300

    Adicionado container technologies

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,13 @@
 import '../styles/components/maincontent.sass';
+import TechnologiesContainer from './TechnologiesContainer';
 
 const MainContent = () => {
   return (
     <main id="main-content">
-      <p>maincontent</p>
+
+      <p>About</p>
+      <TechnologiesContainer />
+      <p>Projects</p>
     </main>
   );
 };
