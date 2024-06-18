8606810f505932e827a024bc69fe55b2601f9c18
Gustavo Carvalho <gustavohs.carvalho280590@gmail.com>
2024-06-18 20:22:56 -0300

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,17 @@
+import SocialNetworks from './SocialNetworks';
+import InformationContainer from './InformationContainer';
 import Avatar from '../img/Selfie.jpg'
 
 import '../styles/components/sidebar.sass';
 
+
 const Sidebar = () => {
   return (
     <aside id="sidebar">
-      <img src={Avatar} alt="Matheus Battisti" />
+      <img src={Avatar} alt="Gustavo Perfil" />
       <p className="title">Desenvolvedor</p>
-      <p>Social</p>
-      <p>Informaçoes</p>
+      <SocialNetworks/>
+      <InformationContainer/>
       <a href="#" className="btn">
         Download currículo
       </a>
